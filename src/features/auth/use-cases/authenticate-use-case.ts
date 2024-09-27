8a4f8889fe57b215b4ab6975e6e74153208949c4
edicleoline <edicleo@proton.me>
2024-09-27 11:49:30 -0300

    ✨ Kickoff: Initialized project with core setup and dependencies

--- src/features/auth/use-cases/authenticate-use-case.ts
+++ src/features/auth/use-cases/authenticate-use-case.ts
@@ -5,12 +5,6 @@ import { AuthDataSource } from '../data-sources/auth-data-source'
 import { UserEntity } from '../../user/entities/user-entity'
 import { AuthenticateResponseEntity } from '../entities/authenticate-response-entity'
 
-interface AuthenticateParameter {
-  token: string
-  password?: string
-  user?: UserEntity
-}
-
 @injectable()
 export default class AuthenticateUseCase extends UseCase<AuthenticateParameter, AuthenticateResponseEntity | null> {
   constructor(
@@ -23,4 +17,10 @@ export default class AuthenticateUseCase extends UseCase<AuthenticateParameter, 
   protected async execute(parameters: AuthenticateParameter): Promise<AuthenticateResponseEntity | null> {
     return await this.remoteAuthDataSource.authenticate(parameters.token, parameters.password, parameters.user)
   }
+}
+
+interface AuthenticateParameter {
+  token: string
+  password?: string
+  user?: UserEntity
 }
